fd5b90f8fb0a61e982da1a89b2675f3efcfd509b
FaihaNiyas <120676945+FaihaNiyas@users.noreply.github.com>
2026-06-01 17:29:38 +0530

    Update auth API, user model, and routes

--- routes/api.php
+++ routes/api.php
@@ -4,15 +4,19 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
+// TEST ROUTE
 Route::get('/test', function () {
     return response()->json([
         'message' => 'Takitro API working'
     ]);
 });
 
+// AUTH ROUTES
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// PROTECTED ROUTES
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
